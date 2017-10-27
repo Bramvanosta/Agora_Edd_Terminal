9d49c163c2c33e8f770e7b6cfe3d00c1997e5817
mpatrois <emmanuel.patrois@gmail.com>
2017-10-27 04:28:53 +0200

    style

--- src/components/chat/OptionSelect.js
+++ src/components/chat/OptionSelect.js
@@ -14,7 +14,7 @@ class OptionSelect extends Component {
     return (
       <View style={styles.optionsContainer}>
         { options.map((option) => (
-          <TouchableOpacity key={option.option_id} onPress={() => this.handlePress(option.option_id, option.name)}>
+          <TouchableOpacity key={option.option_id} onPress={() => this.handlePress(option.option_id, option.name)} style={styles.optionItem} >
             <Text>{ option.name }</Text>
           </TouchableOpacity>
         ))}
@@ -32,6 +32,15 @@ export default OptionSelect;
 
 const styles = StyleSheet.create({
   optionsContainer: {
-    flexDirection: 'row'
+    flexDirection: 'row',
+    flex:1,
+    justifyContent: 'space-between',
+    
+  },
+  optionItem:{
+    color:"#006ce2",
+    padding:20,
+    fontWeight: 500
   }
+
 });
